d55ee21441956de122bd1a692d4f2d88c080a475
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2021-02-15 18:50:05 +0530

    add product component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, AddProductComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
