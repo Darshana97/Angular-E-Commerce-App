dd936a24e3c210d7e4290290db8f0fd107ab44ee
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2021-02-15 18:53:45 +0530

    add view product component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,10 +5,11 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ViewProductByCategoryComponent } from './view-product-by-category/view-product-by-category.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 
 @NgModule({
-  declarations: [ProductsComponent, AddProductComponent, ViewProductByCategoryComponent],
+  declarations: [ProductsComponent, AddProductComponent, ViewProductByCategoryComponent, ViewProductComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
