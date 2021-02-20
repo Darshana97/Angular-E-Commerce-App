9cba419535ae8f4a9fe184e420419c3c0bd6e2c1
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2021-02-20 11:31:13 +0530

    sidebar

--- src/app/site-layout/sidebar/sidebar.component.ts
+++ src/app/site-layout/sidebar/sidebar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/products/product.service';
+import { Category } from '../category';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  categoryList: Category;
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productService.getCategory().subscribe((data) => {
+      this.categoryList = data;
+    });
   }
-
 }
